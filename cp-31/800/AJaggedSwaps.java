26efeb31f0dae09bc2378ce2319d0acaf86a928c
Al-Roman <alroman121523@gmail.com>
2026-08-01 11:59:09 -0400

    chore: add Codeforces 1896A solution

---
+++ cp-31/800/AJaggedSwaps.java
@@ -0,0 +1,36 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+public class AJaggedSwaps
+{
+    public static void main(String[] args) throws java.lang.Exception
+    {
+        Scanner sc = new Scanner(System.in);
+
+        int t = sc.nextInt();
+
+        while (t-- > 0)
+        {
+            int n = sc.nextInt();
+
+            int[] a = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                a[i] = sc.nextInt();
+            }
+
+            if (a[0] == 1)
+            {
+                System.out.println("YES");
+            }
+            else
+            {
+                System.out.println("NO");
+            }
+        }
+
+        sc.close();
+    }
+}
