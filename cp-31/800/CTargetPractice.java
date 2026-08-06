f718f4f0d8f631e30c6cc1a12bf7b4b55928c1be
Al-Roman <alroman121523@gmail.com>
2026-08-06 05:30:22 -0400

    chore: add Codeforces 1873C solution

---
+++ cp-31/800/CTargetPractice.java
@@ -0,0 +1,57 @@
+import java.util.*;
+
+public class ATargetPractice
+{
+    private static final int[][] score =
+    {
+        {1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+        {1, 2, 2, 2, 2, 2, 2, 2, 2, 1},
+        {1, 2, 3, 3, 3, 3, 3, 3, 2, 1},
+        {1, 2, 3, 4, 4, 4, 4, 3, 2, 1},
+        {1, 2, 3, 4, 5, 5, 4, 3, 2, 1},
+        {1, 2, 3, 4, 5, 5, 4, 3, 2, 1},
+        {1, 2, 3, 4, 4, 4, 4, 3, 2, 1},
+        {1, 2, 3, 3, 3, 3, 3, 3, 2, 1},
+        {1, 2, 2, 2, 2, 2, 2, 2, 2, 1},
+        {1, 1, 1, 1, 1, 1, 1, 1, 1, 1}
+    };
+
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+
+        int T = sc.nextInt();
+
+        while (T-- > 0)
+        {
+            char[][] a = new char[10][10];
+
+            for (int i = 0; i < 10; i++)
+            {
+                String s = sc.next();
+
+                for (int j = 0; j < 10; j++)
+                {
+                    a[i][j] = s.charAt(j);
+                }
+            }
+
+            int totalScore = 0;
+
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    if (a[i][j] == 'X')
+                    {
+                        totalScore += score[i][j];
+                    }
+                }
+            }
+
+            System.out.println(totalScore);
+        }
+
+        sc.close();
+    }
+}
